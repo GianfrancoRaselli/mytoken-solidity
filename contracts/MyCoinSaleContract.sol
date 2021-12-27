ac5a3bbd8ab8420f2db19607aafdd40bdeda44f5
Gianfranco Raselli <66041020+GianfrancoRaselli@users.noreply.github.com>
2021-12-27 14:10:06 -0300

    sale contract

--- contracts/MyCoinSaleContract.sol
+++ contracts/MyCoinSaleContract.sol
@@ -22,4 +22,28 @@ contract MyCoinSaleContract {
     myCoinInterface = MyCoinInterface(_addressContract);
   }
 
+  function mul(uint256 a, uint256 b) internal pure returns (uint256 result) {
+    if (a == b) {
+      return 0;
+    }
+    uint256 c = a * b;
+    require(c / a == b);
+    return c;
+  }
+
+  function buy(uint256 _numTokens) public payable {
+    require(msg.value == mul(price, _numTokens));
+    uint256 scaledAmount = mul(_numTokens, uint256(10) ** myCoinInterface.decimals());
+    require(myCoinInterface.balanceOf(address(this)) >= scaledAmount);
+    tokensSold += _numTokens;
+    require(myCoinInterface.transfer(msg.sender, scaledAmount));
+    emit Sold(msg.sender, _numTokens);
+  }
+
+  function endSold() public {
+    require(msg.sender == owner);
+    require(myCoinInterface.transfer(owner, myCoinInterface.balanceOf(address(this))));
+    payable(msg.sender).transfer(address(this).balance);
+  }
+
 }
